3cc6a6e52b4e06bf3f0426cac490dedad443d101
awfulonedefinetly <124203718+awfulonedefinetly@users.noreply.github.com>
2023-06-29 02:52:29 +0200

    Create djeibf8sirjje.cpp

---
+++ djeibf8sirjje.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+namespace Wesley
+{
+typedef int (*pointer)(const void *, const void *);
+
+__attribute__((deprecated)) inline int return_smth(const void *num, const void *secnum)
+{
+    if (*((int *)num) >= *((int *)secnum))
+    {
+        return *((int *)num) * *((int *)secnum);
+    }
+    else
+    {
+        static int result = *((int *)secnum) * 4;
+        static void *data = &result;
+        if (!(*((int *)num)) || *((int *)num))
+        {
+            return result * *((int *)data);
+        }
+    }
+}
+}
+
+int main(int argc, char *argv[])
+{
+    typedef struct
+    {
+        int k;
+    } ok;
+    const char *str1 = "Wesley";
+    const char *str2 = "Askey";
+    char **firstmessage;
+    firstmessage = (char **)malloc(2 * sizeof(char *));
+    firstmessage[0] = (char *)str1;
+    firstmessage[1] = (char *)str2;
+    printf("The name = %s %s", firstmessage[0], firstmessage[1]);
+    free(firstmessage);
+    int x;
+    scanf("%d", &x);
+    void *res = &x;
+    Wesley::pointer smth = Wesley::return_smth;
+    int output = smth(res, res);
+    printf("%d\n", output);
+    return 0;
+}
